80cbb2c8eae4ec7474f0082ae0067e851f4a1322
Nicolli Santos <168275916+nicollisantos@users.noreply.github.com>
2026-03-04 00:58:19 +0000

    Adiciona operações básicas de matemática e entrada do usuário

---
+++ aula2.py
@@ -0,0 +1,38 @@
+#input
+#variavel = input("Mensagem para o user:")
+#print ("Olá" + nome + "tudo bem com você?")
+
+
+#a = "Nicolli"
+#b = "100"
+#soma = a + b
+#print(soma)
+
+
+#a = int(input("digite a:"))
+#b = int(input("digite b:"))
+#resultado = a % b
+#print(resultado)
+
+
+#potenciação (**)
+#potenciação = 2 ** 3
+#print("potenciação", potenciação)
+
+#divisao inteira (//)
+#divisão_inteiro = 10 / 2
+#print("divisão_inteiro", divisão_inteiro)
+
+#ATIVIDADE TEAMS:
+
+a = int(input("digite a:"))
+b = int(input("digite b:"))
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão = a / b
+
+print("SOMA:", soma)
+print("SUBTRAÇÃO:", subtração)
+print("MULTIPLICAÇÃO:", multiplicação)
+print("DIVISÃO:", divisão)
