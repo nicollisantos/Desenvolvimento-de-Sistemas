e65d659ba43545620a45d44e3a69e417ed30b447
Nicolli Santos <168275916+nicollisantos@users.noreply.github.com>
2026-03-04 00:59:19 +0000

    Implementa cálculo de soma com três números e exibe resultado

---
+++ atv1teams.py
@@ -0,0 +1,26 @@
+#ATIVIDADE TEAMS:
+
+#a = int(input("digite a:"))
+#b = int(input("digite b:"))
+#soma = a + b
+#subtração = a - b
+#multiplicação = a * b
+#divisão = a / b
+
+#print("SOMA:", soma)
+#print("SUBTRAÇÃO:", subtração)
+#print("MULTIPLICAÇÃO:", multiplicação)
+#print("DIVISÃO:", divisão)
+
+
+# PRIMEIRO CÓDIGO
+
+print("digite para SOMA:")
+a = int(input("digite o primeiro número: "))
+print()
+b = int(input("digite o segundo número: "))
+print()
+c = int(input("digite o terceiro número: "))
+soma = ( a + b - c) / 2
+print()
+print("Seu resultado é: ", soma)
